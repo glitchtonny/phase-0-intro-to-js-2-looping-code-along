9fdc2792e119247a042d5a0266a61fe814aa210c
g;itchtonny <sirmwangitosh@gmail.com>
2024-04-03 14:44:57 +0300

    looping lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(george, birthday) {
+    let cardsArray = [];
+    for (let i = 0; i < george.length; i++) {
+        cardsArray.push(`Thank you, ${george[i]}, for the wonderful ${birthday} gift!`);
+    }
+    return cardsArray;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
